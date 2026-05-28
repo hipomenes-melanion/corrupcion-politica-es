76b30c0143036bd69cb0f209bef00ec9561c7e95
José Manuel Montero Durán <jmmontero@telefonica.net>
2026-05-28 10:57:32 +0200

    Quartz sync: May 28, 2026, 10:57 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,23 +1,16 @@
 import { QuartzConfig } from "./quartz/cfg"
 import * as Plugin from "./quartz/plugins"
 
-/**
- * Quartz 4 Configuration
- *
- * See https://quartz.jzhao.xyz/configuration for more information.
- */
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "Corrupción Política ES",
-    pageTitleSuffix: "",
-    enableSPA: true,
-    enablePopovers: true,
+    provider: "local",
+    locale: "es-ES",
+    baseUrl: "hipomenes-melanion.github.io/corrupcion-politica-es",
+    ignorePatterns: [".obsidian", ".trash", "__tests__", "nodes_modules"],
     analytics: {
       provider: "plausible",
     },
-    locale: "es-ES",
-    baseUrl: "hipomenes-melanion.github.io/corrupcion-politica-es",
-    ignorePatterns: ["private", "templates", ".obsidian"],
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
@@ -27,27 +20,28 @@ const config: QuartzConfig = {
         code: "IBM Plex Mono",
       },
       colors: {
-        lightMode: {
-          light: "#faf8f8",
-          lightgray: "#e5e5e5",
-          gray: "#b8b8b8",
-          darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
-          secondary: "#284b63",
+        light: {
+          page: "#faf8f8",
+          pageborder: "#e4e4e7",
+          background: "#faf8f8",
+          sidebar: "#f4f4f5",
+          body: "#27272a",
+          header: "#18181b",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
-          textHighlight: "#fff23688",
+          textHighlight: "#fff275",
         },
-        darkMode: {
-          light: "#161618",
-          lightgray: "#393639",
-          gray: "#646464",
-          darkgray: "#d4d4d4",
-          dark: "#ebebec",
-          secondary: "#7b97aa",
+        dark: {
+          page: "#161618",
+          pageborder: "#27272a",
+          background: "#161618",
+          sidebar: "#1e1e20",
+          body: "#d4d4d8",
+          header: "#f4f4f5",
+          secondary: "hsl(255, 90%, 76%)", 
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
-          textHighlight: "#b3aa0288",
+          textHighlight: "#b3aa02",
         },
       },
     },
@@ -55,24 +49,14 @@ const config: QuartzConfig = {
   plugins: {
     transformers: [
       Plugin.FrontMatter(),
-      Plugin.CreatedModifiedDate({
-        priority: ["frontmatter", "git", "filesystem"],
-      }),
-      Plugin.SyntaxHighlighting({
-        theme: {
-          light: "github-light",
-          dark: "github-dark",
-        },
-        keepBackground: false,
-      }),
+      Plugin.CreatedModifiedDate(),
+      Plugin.SyntaxHighlighting(),
       Plugin.ObsidianFlavoredMarkdown({
         enableInHtmlEmbed: true,
         mermaid: true,
       }),
       Plugin.GitHubFlavoredMarkdown(),
-      Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
-      Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
     ],
     filters: [Plugin.RemoveDrafts()],
@@ -88,10 +72,7 @@ const config: QuartzConfig = {
       }),
       Plugin.Assets(),
       Plugin.Static(),
-      Plugin.Favicon(),
       Plugin.NotFoundPage(),
-      // Comment out CustomOgImages to speed up build time
-      Plugin.CustomOgImages(),
     ],
   },
 }
