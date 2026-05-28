fe38218e165fa7a8286a9b1de6198e0b15a695b5
José Manuel Montero Durán <jmmontero@telefonica.net>
2026-05-28 10:30:02 +0200

    Quartz sync: May 28, 2026, 10:30 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -65,7 +65,10 @@ const config: QuartzConfig = {
         },
         keepBackground: false,
       }),
-      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+      Plugin.ObsidianFlavoredMarkdown({
+        enableInHtmlEmbed: true,
+        mermaid: true,
+      }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
